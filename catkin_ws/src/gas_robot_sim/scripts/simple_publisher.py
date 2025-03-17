0cd94d61b52ff2026c2ef8217ed4083e7d1f4126
huy7 <huypq72@gmail.com>
2025-03-17 01:51:56 +0000

    recode simple node & simple publisher

--- catkin_ws/src/gas_robot_sim/scripts/simple_publisher.py
+++ catkin_ws/src/gas_robot_sim/scripts/simple_publisher.py
@@ -4,12 +4,13 @@ import rospy
 from std_msgs.msg import String
 
 def main():
+    #init node
     rospy.init_node('simple_publisher')
-    pub = rospy.Publisher('simple_topic', String, queue_size=10)
+    pub = rospy.Publisher('/simple_topic', String, queue_size=10)
     rate = rospy.Rate(0.5)
     while not rospy.is_shutdown():
-        pub.publish('Hello World!')
-        rospy.loginfo('Publishing message at: %s', rospy.Time.now())
+        pub.publish('Hello World')
+        rospy.loginfo('Message sent at %s', rospy.get_time())
         rate.sleep()
 
 if __name__ == '__main__':
--- catkin_ws/src/gas_robot_sim/scripts/simple_subscriber.py
+++ catkin_ws/src/gas_robot_sim/scripts/simple_subscriber.py
@@ -3,12 +3,12 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(msg):  
-    rospy.loginfo('Received message: %s', msg.data)
-    
+def callback(msg):
+    rospy.loginfo('Message received: %s', msg.data)
+
 def main():
     rospy.init_node('simple_subscriber')
-    rospy.Subscriber('simple_topic', String, callback)
+    sub = rospy.Subscriber('simple_topic', String, callback)
     rospy.spin()
 
 if __name__ == '__main__':
